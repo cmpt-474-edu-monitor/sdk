3ba5d57d498d0c3466ce13b96b1d494fc0f6182d
tabjy <admin@tabjy.com>
2021-03-26 03:34:41 -0700

    Support session. Propagate error name property
    - the first 'context' argument is actually the lambda context now
    - error.name is now propagated to the front end
    - added support for persistent session data
    -- the session object is mounted as 'context.session'
    -- any modification to the session object will be serialised, encrypted, and stored by client
    -- no change to SDK interfaces (session data is not accessible on client due to security concerns)

--- index.js
+++ index.js
@@ -5,16 +5,5 @@ const Client = require('./lib/client')
 module.exports = {
   GateWayBuilder,
   ServiceBuilder,
-  Client,
-
-  // development: API gateway
-  // handler: new GateWayBuilder().addNamespace('Tests').build()
-
-
-  // development: service function
-  // handler: new ServiceBuilder()
-  //   .addInterface('echo', (ctx, ...args) => args)
-  //   .addInterface('getAnswer', (ctx) => 42)
-  //   .addInterface('add', (ctx, a, b) => a + b)
-  //   .build()
+  Client
 }
